5f6ac4d45d5587067c477c54bba7db4e0cb1c7e5
Arati <aratilangote06@gmail.com>
2022-11-05 13:41:46 +0530

    AdapterViewFlipper

--- app/src/main/java/com/capstoneProject/grapebhumi/HomeFragment.java
+++ app/src/main/java/com/capstoneProject/grapebhumi/HomeFragment.java
@@ -1,12 +1,19 @@
 package com.capstoneProject.grapebhumi;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterViewFlipper;
+import android.widget.BaseAdapter;
+import android.widget.CursorAdapter;
+import android.widget.ImageView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +21,12 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class HomeFragment extends Fragment {
+
+    //AdapterViewFlipper
+    AdapterViewFlipper adaptFlipper;
+    private static final int[] images = {R.drawable.farm, R.drawable.grape_farm, R.drawable.grapegrowth, R.drawable.grape_types};
+
+
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -47,7 +60,7 @@ public class HomeFragment extends Fragment {
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)  {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
@@ -58,7 +71,52 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+
+
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        //AdapterViewFlipper
+        adaptFlipper = view.findViewById(R.id.adptflipper);
+        ViewFlipperAdapter viewFlipper = new ViewFlipperAdapter(getContext(), images);
+        adaptFlipper.setAdapter(viewFlipper);
+        adaptFlipper.setFlipInterval(1000);
+        adaptFlipper.setAutoStart(true);
+    }
+
+    //AdapterViewFlipper viewFlipper custom class
+    class ViewFlipperAdapter extends BaseAdapter{
+        Context context;
+        int[] images;
+        LayoutInflater inflater;
+
+        public ViewFlipperAdapter(Context mycontext, int[] myimages){
+            this.context = mycontext;
+            this.images = myimages;
+            inflater = LayoutInflater.from(mycontext);
+        }
+
+        @Override
+        public int getCount() {return 4;}
+
+        @Override
+        public Object getItem(int i) {return null;}
+
+        @Override
+        public long getItemId(int i) {return 0;}
+
+        @Override
+        public View getView(int position, View view, ViewGroup viewGroup) {
+            view = inflater.inflate(R.layout.activity_adapterimages, null);
+
+            ImageView imageView = view.findViewById(R.id.flip_imgs);
+            imageView.setImageResource(images[position]);
+            return view;
+        }
+    }
+
 }
